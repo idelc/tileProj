6e2341ca01734250d15eceb62413b4587e068259
idelc <idel012@ucr.edu>
2021-10-31 17:57:20 -0700

    added move up feature and testing

--- main.cpp
+++ main.cpp
@@ -5,11 +5,36 @@ using namespace std;
 #include "puzzleMaker.h"
 
 int main(){
-    Puzzle pzlToUse(3);
+    //test init
+    unsigned pzlSize = 3;
+    Puzzle pzlToUse(pzlSize);
     cout << pzlToUse << endl;
-    pzlToUse.editPuzzle(0,0,5);
+    // pzlToUse.editPuzzle(0,0,5);
+    // cout << pzlToUse << endl;
+    // pzlToUse.clearPuzzle();
+    // cout << pzlToUse << endl;   
+
+    // test movement
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+    pzlToUse.moveUp();
     cout << pzlToUse << endl;
-    pzlToUse.clearPuzzle();
-    cout << pzlToUse << endl;    
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+    pzlToUse.moveUp();
+    cout << pzlToUse << endl;
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+    pzlToUse.moveUp();
+    cout << pzlToUse << endl;
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+    pzlToUse.moveUp();
+    cout << pzlToUse << endl;
+    cout << "\nZero pos: " << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1] << endl;
+    
+
     return 0;
 }
--- puzzleMaker.h
+++ puzzleMaker.h
@@ -8,7 +8,8 @@ class Puzzle{
  public:
     Puzzle(unsigned sz): size(sz){ // pass in 3 for an 8 puzzle, hardcoded for now
         // cout << "filling in puzzle" << endl;
-
+        zeroPos[0] = sz - 1;
+        zeroPos[1] = sz - 1;
         unsigned n = 1;
         for(unsigned i = 0; i < this->size; i++){
             for(unsigned j = 0; j < this->size; j++){
@@ -45,6 +46,14 @@ class Puzzle{
         this->pzlBoard[x][y] = num;
     }
 
+    unsigned moveUp(){
+        if(this->zeroPos[0] == 0){return 0;}
+        this->pzlBoard[this->zeroPos[0]][this->zeroPos[1]] = this->pzlBoard[this->zeroPos[0]-1][this->zeroPos[1]];
+        this->pzlBoard[this->zeroPos[0]-1][this->zeroPos[1]] = 0;
+        this->zeroPos[0] = this->zeroPos[0] - 1;
+        return 1;
+    }
+
     // ~Puzzle(){
     //     for(int i = 0; i < this->size; i++){
     //         delete[] this->pzlBoard[i];
@@ -53,6 +62,7 @@ class Puzzle{
     // }
     
     unsigned size; // puzzle of size x size, will test with 8
+    unsigned zeroPos[2];
     unsigned pzlBoard [3][3];
 };
 
