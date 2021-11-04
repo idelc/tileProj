93d24ef28d93f7b89b07703ef9c69a474fd3932c
idelc <idel012@ucr.edu>
2021-11-03 18:45:14 -0700

    manhattan works up to depth 20?

--- algorithms.h
+++ algorithms.h
@@ -17,7 +17,7 @@ const Puzzle SOLVED_PUZZLE(3);
 
 class Node{ // struct to hold node info
  public:
-    Node(Puzzle& pzl, unsigned cst, unsigned heu):nodePzl(pzl),cost(cst),heuristic(heu){};
+    Node(Puzzle& pzl, unsigned cst, unsigned heu, unsigned lastM):nodePzl(pzl),cost(cst),heuristic(heu), lastMove(lastM){};
     Node(const Node& nd): nodePzl(nd.nodePzl), cost(nd.cost), heuristic(nd.heuristic), lastMove(nd.lastMove){};
     ~Node(){}
     Puzzle nodePzl;
@@ -26,86 +26,86 @@ class Node{ // struct to hold node info
     unsigned lastMove; // store last move and dont make a node undoing it
 };
 
-bool operator<(const Node& p1, const Node& p2){ //overload comparison to allow priority queue to sort properly
+bool operator>(const Node& p1, const Node& p2){ //overload comparison to allow priority queue to sort properly
         int f1 = (p1.cost + p1.heuristic); 
         int f2 = (p2.cost + p2.heuristic);
-        return f1 < f2;
+        return f1 > f2;
 }
 
-void uniformCostSearch(Puzzle& pzl){
-    priority_queue<Node*, vector<Node*>, std::greater<Node*>> nodes; // min priority queue 
-    nodes.push(new Node(pzl,0,0));
-    bool solved = false; // loop condition
-    // int times = 0;
-    // ofstream write;
-    // write.open("UCtrace.txt");
-    // if(!write.is_open()){
-    //     cout << "error opening file" << endl;
-    // }
-    while(!solved){
-        if(nodes.empty()){
-            cout << "Failed to solve the puzzle" << endl;
-            return;
-        }
-        Node* temp = nodes.top();
-        nodes.pop();
-        // write << temp->nodePzl << "cost: " << temp->cost << " in queue: " << nodes.size() << endl;
-        bool match = true;
-        unsigned nodePzlSize = temp->nodePzl.size;
-        for(unsigned i = 0; i < nodePzlSize; i++){
-            for(unsigned j = 0; j < nodePzlSize; j++){
-                // is the temp node puzzle at the current value the same as the solved puzzle at that value?
-                // asume true, if find one discrepancy set flag to false for remainder of loop
-                if(temp->nodePzl.pzlBoard[i][j] != SOLVED_PUZZLE.pzlBoard[i][j]){
-                    match = false;
-                }
-            }
-        }
-        if(match){
-            cout << "solution found at depth " << temp->cost << endl;
-            return;
-        }
-        else{
-            for(unsigned i = 0; i < 4; i++){
-                Node tempMoves = *temp;
-                switch (i){
-                case 0:
-                    tempMoves.nodePzl.moveUp();
-                    tempMoves.cost++;
-                    nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                    //cout << "up" << endl;
-                    break;
+// void uniformCostSearch(Puzzle& pzl){
+//     priority_queue<Node*, vector<Node*>, std::greater<Node*>> nodes; // min priority queue 
+//     nodes.push(new Node(pzl,0,0));
+//     bool solved = false; // loop condition
+//     // int times = 0;
+//     // ofstream write;
+//     // write.open("UCtrace.txt");
+//     // if(!write.is_open()){
+//     //     cout << "error opening file" << endl;
+//     // }
+//     while(!solved){
+//         if(nodes.empty()){
+//             cout << "Failed to solve the puzzle" << endl;
+//             return;
+//         }
+//         Node* temp = nodes.top();
+//         nodes.pop();
+//         // write << temp->nodePzl << "cost: " << temp->cost << " in queue: " << nodes.size() << endl;
+//         bool match = true;
+//         unsigned nodePzlSize = temp->nodePzl.size;
+//         for(unsigned i = 0; i < nodePzlSize; i++){
+//             for(unsigned j = 0; j < nodePzlSize; j++){
+//                 // is the temp node puzzle at the current value the same as the solved puzzle at that value?
+//                 // asume true, if find one discrepancy set flag to false for remainder of loop
+//                 if(temp->nodePzl.pzlBoard[i][j] != SOLVED_PUZZLE.pzlBoard[i][j]){
+//                     match = false;
+//                 }
+//             }
+//         }
+//         if(match){
+//             cout << "solution found at depth " << temp->cost << endl;
+//             return;
+//         }
+//         else{
+//             for(unsigned i = 0; i < 4; i++){
+//                 Node tempMoves = *temp;
+//                 switch (i){
+//                 case 0:
+//                     tempMoves.nodePzl.moveUp();
+//                     tempMoves.cost++;
+//                     nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
+//                     //cout << "up" << endl;
+//                     break;
                 
-                case 1:
-                    tempMoves.nodePzl.moveLeft();
-                    tempMoves.cost++;
-                    nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                    //cout << "left" << endl;
-                    break;
+//                 case 1:
+//                     tempMoves.nodePzl.moveLeft();
+//                     tempMoves.cost++;
+//                     nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
+//                     //cout << "left" << endl;
+//                     break;
 
-                case 2: 
-                    tempMoves.nodePzl.moveRight();
-                    tempMoves.cost++;
-                    nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                    //cout << "right" << endl;
-                    break;
+//                 case 2: 
+//                     tempMoves.nodePzl.moveRight();
+//                     tempMoves.cost++;
+//                     nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
+//                     //cout << "right" << endl;
+//                     break;
 
-                case 3:
-                    tempMoves.nodePzl.moveDown();
-                    tempMoves.cost++;
-                    nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                    //cout << "down" << endl;
-                    break;
+//                 case 3:
+//                     tempMoves.nodePzl.moveDown();
+//                     tempMoves.cost++;
+//                     nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
+//                     //cout << "down" << endl;
+//                     break;
 
-                default:
-                    cout << "error in expanding UC children" << endl;
-                    exit(1);
-                    break;
-                }
-            }
-        }
-    }
-}
+//                 default:
+//                     cout << "error in expanding UC children" << endl;
+//                     exit(1);
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+// }
 
 
 
@@ -123,7 +123,7 @@ unsigned misplacedTile(Puzzle& currPz){
     return mT;    
 }
 
-unsigned manhattan(Puzzle& currPz){
+unsigned manhattan(const Puzzle& currPz){
     unsigned sz = currPz.size;
     unsigned manhArr [(sz*sz)][2];
     unsigned arrPnt = 1;
@@ -151,26 +151,24 @@ unsigned manhattan(Puzzle& currPz){
 // URGENT TODO: Note that if a move is invalid, no node should be created
 // in node, store last move made to avoid undoing move
 void aStar(Puzzle& pzl, unsigned mh){
-    priority_queue<Node*> nodes; // min priority queue 
-    Node* firstNode = new Node(pzl,0,0);
-    firstNode->lastMove = 4; // set so that nothing is excluded from first expansion
-    nodes.push(firstNode);
+    priority_queue<Node*, vector <Node*>, std::greater<Node*> > nodes; // min priority queue 
+    nodes.push(new Node(pzl,0,0,4)); //First node, no cost, no heuristic, last move is invalid
     bool solved = false; // loop condition
     // int times = 0;
-    ofstream write;
-    write.open("Manhtrace.txt");
-    if(!write.is_open()){
-        cout << "error opening file" << endl;
-    }
+    // ofstream write;
+    // write.open("Manhtrace.txt");
+    // if(!write.is_open()){
+    //     cout << "error opening file" << endl;
+    // }
     while(!solved){
         if(nodes.empty()){
             cout << "Failed to solve the puzzle" << endl;
             return;
         }
-        Node temp = *nodes.top();
-        nodes.pop();
+        Node temp = *nodes.top(); // make a copy so as to not need top directly
+        nodes.pop(); // delete top
         
-        write << "\n\n" << temp.nodePzl << "cost: " << temp.cost << " heu: " << temp.heuristic <<" in queue: " << nodes.size() << "\n" << endl;
+        // write << "\n\n" << temp.nodePzl << "cost: " << temp.cost << " heu: " << temp.heuristic <<" in queue: " << nodes.size() << "\n" << endl;
         bool match = true;
         unsigned nodePzlSize = temp.nodePzl.size;
         for(unsigned i = 0; i < nodePzlSize; i++){
@@ -188,62 +186,63 @@ void aStar(Puzzle& pzl, unsigned mh){
         }
         else{
             Node tempMoves = temp;
-            bool validMove = false;
+            unsigned int validMove = 0;
             for(unsigned i = 0; i < 4; i++){
                 tempMoves = temp;
+                validMove = false;
                 switch (i){
-                case 0:
-                    if(tempMoves.lastMove == 3) {break;}
-                    tempMoves.lastMove = 0;
-                    validMove = tempMoves.nodePzl.moveUp();
-                    tempMoves.cost++;
-                    tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
-                    if(validMove){
-                        nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                        write << "up\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
-                    }
-                    break;
-                
-                case 1:
-                    if(tempMoves.lastMove == 2) {break;}
-                    tempMoves.lastMove = 1;
-                    validMove = tempMoves.nodePzl.moveLeft();
-                    tempMoves.cost++;
-                    tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
-                    if(validMove){
-                        nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                        write << "left\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
-                    }
-                    break;
+                    case 0:
+                        if(tempMoves.lastMove == 3) {break;}
+                        tempMoves.lastMove = 0;
+                        tempMoves.cost = tempMoves.cost + 1;
+                        validMove = tempMoves.nodePzl.moveUp();
+                        tempMoves.heuristic = (mh) ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
+                        if(validMove){
+                            nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic, tempMoves.lastMove));
+                            // write << "up\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
+                        }
+                        break;
+                    
+                    case 1:
+                        if(tempMoves.lastMove == 2) {break;}
+                        tempMoves.lastMove = 1;
+                        tempMoves.cost = tempMoves.cost + 1;
+                        validMove = tempMoves.nodePzl.moveLeft();
+                        tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
+                        if(validMove){
+                            nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic, tempMoves.lastMove));
+                            // write << "left\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
+                        }
+                        break;
 
-                case 2: 
-                    if(tempMoves.lastMove == 1) {break;}
-                    tempMoves.lastMove = 2;
-                    validMove = tempMoves.nodePzl.moveRight();
-                    tempMoves.cost++;
-                    tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
-                    if(validMove){
-                        nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                        write << "right\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
-                    }
-                    break;
+                    case 2: 
+                        if(tempMoves.lastMove == 1) {break;}
+                        tempMoves.lastMove = 2;
+                        tempMoves.cost = tempMoves.cost + 1;
+                        validMove = tempMoves.nodePzl.moveRight();
+                        tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
+                        if(validMove){
+                            nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic, tempMoves.lastMove));
+                            // write << "right\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
+                        }
+                        break;
 
-                case 3:
-                    if(tempMoves.lastMove == 0) {break;}
-                    tempMoves.lastMove = 3;
-                    validMove = tempMoves.nodePzl.moveDown();
-                    tempMoves.cost++;
-                    tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
-                    if(validMove){
-                        nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic));
-                        write << "down\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
-                    }
-                    break;
+                    case 3:
+                        if(tempMoves.lastMove == 0) {break;}
+                        tempMoves.lastMove = 3;
+                        tempMoves.cost = tempMoves.cost + 1;
+                        validMove = tempMoves.nodePzl.moveDown();
+                        tempMoves.heuristic = mh ? manhattan(tempMoves.nodePzl) : misplacedTile(tempMoves.nodePzl);
+                        if(validMove){
+                            nodes.push(new Node(tempMoves.nodePzl, tempMoves.cost, tempMoves.heuristic, tempMoves.lastMove));
+                            // write << "down\n" << tempMoves.nodePzl << "cost: " << tempMoves.cost << " heu: " << tempMoves.heuristic << " in queue: " << nodes.size() << endl;
+                        }
+                        break;
 
-                default:
-                    cout << "error in expanding UC children" << endl;
-                    exit(1);
-                    break;
+                    default:
+                        cout << "error in expanding UC children" << endl;
+                        exit(1);
+                        break;
                 }
             }
         }
--- main.cpp
+++ main.cpp
@@ -9,22 +9,22 @@ int main(){
     //test init
     unsigned pzlSize = 3;
     Puzzle pzlToUse(pzlSize);
-    pzlToUse.editPuzzle(0,0,1);
-    pzlToUse.editPuzzle(0,1,2);
-    pzlToUse.editPuzzle(0,2,3);
+    pzlToUse.editPuzzle(0,0,7);
+    pzlToUse.editPuzzle(0,1,1);
+    pzlToUse.editPuzzle(0,2,2);
     pzlToUse.editPuzzle(1,0,4);
-    pzlToUse.editPuzzle(1,1,5);
-    pzlToUse.editPuzzle(1,2,6);
-    pzlToUse.editPuzzle(2,0,0);
-    pzlToUse.editPuzzle(2,1,7);
-    pzlToUse.editPuzzle(2,2,8);
-    pzlToUse.zeroPos[0] = 2;
-    pzlToUse.zeroPos[1] = 0;
+    pzlToUse.editPuzzle(1,1,8);
+    pzlToUse.editPuzzle(1,2,5);
+    pzlToUse.editPuzzle(2,0,6);
+    pzlToUse.editPuzzle(2,1,3);
+    pzlToUse.editPuzzle(2,2,0);
+    pzlToUse.zeroPos[0] = 1;
+    pzlToUse.zeroPos[1] = 1;
 
     cout << pzlToUse << pzlToUse.zeroPos[0] << ", " << pzlToUse.zeroPos[1]<< endl;
 
     cout << "testing using AStar" << endl;
-    cout << "depth 7\n" << endl;
+    cout << "depth 20\n" << endl;
 
     aStar(pzlToUse, 1);
     
